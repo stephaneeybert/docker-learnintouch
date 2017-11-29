f400cc0733a67e7747ae38b24e036714b3afd0c0
Stephane Eybert <mittiprovence@yahoo.se>
2017-11-29 23:56:45 +0200

    Removed white spaces.

--- redis/build.sh
+++ redis/build.sh
@@ -2,8 +2,8 @@
 
 docker build -t stephaneeybert/redis .
 if [ $? -eq 0 ]; then
-docker tag stephaneeybert/nodejs-learnintouch localhost:5000/redis
+docker tag stephaneeybert/redis localhost:5000/redis
 docker push localhost:5000/redis
-docker tag stephaneeybert/nodejs-learnintouch thalasoft.com:5000/redis
+docker tag stephaneeybert/redis thalasoft.com:5000/redis
 docker push thalasoft.com:5000/redis
 fi
