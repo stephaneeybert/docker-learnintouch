7845d6969d2d862f94be43fb24af469873cef3ae
Stephane Eybert <mittiprovence@yahoo.se>
2018-05-08 15:00:46 +0200

    Corrected the name of the image to tag.

--- www.folkuniversitet/build.sh
+++ www.folkuniversitet/build.sh
@@ -2,8 +2,8 @@
 
 docker build -t stephaneeybert/www.folkuniversitet .
 if [ $? -eq 0 ]; then
-docker tag stephaneeybert/learnintouch.com localhost:5000/www.folkuniversitet
+docker tag stephaneeybert/www.folkuniversitet localhost:5000/www.folkuniversitet
 docker push localhost:5000/www.folkuniversitet
-docker tag stephaneeybert/learnintouch.com thalasoft.com:5000/www.folkuniversitet
+docker tag stephaneeybert/www.folkuniversitet thalasoft.com:5000/www.folkuniversitet
 docker push thalasoft.com:5000/www.folkuniversitet
 fi
