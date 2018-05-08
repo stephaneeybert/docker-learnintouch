9fc22bbb5a57683f688ada35ab2e4527c5954742
Stephane Eybert <mittiprovence@yahoo.se>
2018-05-08 15:02:45 +0200

    Added some help comment.

--- www.learnintouch/website-db-seed.sh
+++ www.learnintouch/website-db-seed.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
+# Give some time to the MySQL server to start before executing the following scripts
 sleep 30s
 
 /usr/bin/mysql/install/bin/mysql --protocol=tcp -h mysql -P 3306 -u root -proot -v < /usr/bin/learnintouch/www/learnintouch.com/db-database.sql
