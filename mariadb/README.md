6d39e9ed06effd17db16d4ec0cfce2321f88707d
Stephane Eybert <mittiprovence@yahoo.se>
2018-05-09 17:36:52 +0200

    Added a secret for the root password of the www.folkuniversitet website.

---
+++ mariadb/README.md
@@ -0,0 +1,3 @@
+The root password is created as a secret
+echo "root" | docker secret create db_root_password -
+
--- www.folkuniversitet/website-db-seed.sh
+++ www.folkuniversitet/website-db-seed.sh
@@ -1,8 +1,15 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # Give some time to the MySQL server to start before executing the following scripts
 sleep 30s
 
-/usr/bin/mariadb/install/bin/mysql --protocol=tcp -h mysql -P 3306 -u root -proot -v < /usr/bin/learnintouch/www/folkuniversitet/db-database.sql
-/usr/bin/mariadb/install/bin/mysql db_folkuniversitet --protocol=tcp -h mysql -P 3306 -u root -proot -v < /usr/bin/learnintouch/www/folkuniversitet/db-structure.sql
-/usr/bin/mariadb/install/bin/mysql db_folkuniversitet --protocol=tcp -h mysql -P 3306 -u root -proot -v < /usr/bin/learnintouch/www/folkuniversitet/db-data.sql
+# Expand the secrets
+export DB_ROOT_PASSWORD={{DOCKER-SECRET:DB_ROOT_PASSWORD}}
+source /usr/bin/learnintouch/expand-secrets.sh
+
+# Avoid having to provide the user password on the command line
+export MYSQL_PWD=$DB_ROOT_PASSWORD
+
+/usr/bin/mariadb/install/bin/mysql --protocol=tcp -h mysql -P 3306 -u root -v < /usr/bin/learnintouch/www/folkuniversitet/db-database.sql
+/usr/bin/mariadb/install/bin/mysql db_folkuniversitet --protocol=tcp -h mysql -P 3306 -u root -v < /usr/bin/learnintouch/www/folkuniversitet/db-structure.sql
+/usr/bin/mariadb/install/bin/mysql db_folkuniversitet --protocol=tcp -h mysql -P 3306 -u root -v < /usr/bin/learnintouch/www/folkuniversitet/db-data.sql
