c4a97045fec130f190a54b2e38f87d3d352782bf
Stephane Eybert <mittiprovence@yahoo.se>
2019-01-28 15:50:33 +0200

    Upgraded to a release supporting sequences.

--- mariadb/build.sh
+++ mariadb/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 
-docker build -t stephaneeybert/mariadb:10.1.24 --rm=true .
+docker build -t stephaneeybert/mariadb:10.3.7 --rm=true .
 if [ $? -eq 0 ]; then
-docker tag stephaneeybert/mariadb:10.1.24 localhost:5000/mariadb:10.1.24
-docker push localhost:5000/mariadb:10.1.24
-docker tag stephaneeybert/mariadb:10.1.24 thalasoft.com:5000/mariadb:10.1.24
-docker push thalasoft.com:5000/mariadb:10.1.24
+docker tag stephaneeybert/mariadb:10.3.7 localhost:5000/mariadb:10.3.7
+docker push localhost:5000/mariadb:10.3.7
+docker tag stephaneeybert/mariadb:10.3.7 thalasoft.com:5000/mariadb:10.3.7
+docker push thalasoft.com:5000/mariadb:10.3.7
 fi
