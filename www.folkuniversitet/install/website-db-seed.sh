beda1c2238892b42af4843be215fc49c06982970
Stephane Eybert <mittiprovence@yahoo.se>
2021-04-26 06:33:12 +0200

    Changed some file paths

--- www.folkuniversitet/install/website-db-seed.sh
+++ www.folkuniversitet/install/website-db-seed.sh
@@ -11,11 +11,11 @@ source /usr/local/learnintouch/expand-secrets.sh
 # Avoid having to provide the user password on the command line
 export MYSQL_PWD=$DB_ROOT_PASSWORD
 
-sed -e "s/DB_PASSWORD/$WWW_FOLKUNIVERSITET_DB_PASSWORD/g" < /usr/local/learnintouch/www/folkuniversitet/db-database.sql > /usr/local/learnintouch/www/folkuniversitet/db-database-resolved.sql
+sed -e "s/DB_PASSWORD/$WWW_FOLKUNIVERSITET_DB_PASSWORD/g" < /usr/local/learnintouch/www/folkuniversitet.europasprak.com/db-database.sql > /usr/local/learnintouch/www/folkuniversitet.europasprak.com/db-database-resolved.sql
 
-/usr/local/mariadb/install/bin/mysql --protocol=tcp -h mysql -P 3306 -u root -v < /usr/local/learnintouch/www/folkuniversitet/db-database-resolved.sql
-/usr/local/mariadb/install/bin/mysql db_folkuniversitet --protocol=tcp -h mysql -P 3306 -u root -v < /usr/local/learnintouch/www/folkuniversitet/db-structure.sql
-/usr/local/mariadb/install/bin/mysql db_folkuniversitet --protocol=tcp -h mysql -P 3306 -u root -v < /usr/local/learnintouch/www/folkuniversitet/db-data.sql
+/usr/local/mariadb/install/bin/mysql --protocol=tcp -h mysql -P 3306 -u root -v < /usr/local/learnintouch/www/folkuniversitet.europasprak.com/db-database-resolved.sql
+/usr/local/mariadb/install/bin/mysql db_folkuniversitet --protocol=tcp -h mysql -P 3306 -u root -v < /usr/local/learnintouch/www/folkuniversitet.europasprak.com/db-structure.sql
+/usr/local/mariadb/install/bin/mysql db_folkuniversitet --protocol=tcp -h mysql -P 3306 -u root -v < /usr/local/learnintouch/www/folkuniversitet.europasprak.com/db-data.sql
 
 chown -R $HOST_USER_ID /usr/local/mariadb/install/data
 chgrp -R $HOST_GROUP_ID /usr/local/mariadb/install/data
